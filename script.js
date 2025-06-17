175cb322d0f0107d619427bc139e00cfac3baa0e
cemen2000 <157874753+cemen2000@users.noreply.github.com>
2025-06-18 02:53:27 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,22 +3,38 @@ document.getElementById('contact-form').addEventListener('submit', function(even
 
     const name = document.getElementById('name').value.trim();
     const email = document.getElementById('email').value.trim();
+    const secondname = document.getElementById('second name').value.trim();
     const message = document.getElementById('message').value.trim();
-
-    // Проверка минимальной длины имени
+    
     if (name.length < 3) {
-        alert('Имя должно быть не менее 3 символов');
-        return;
-    }
+    Swal.fire({
+        icon: 'error',
+        title: 'Ошибка валидации',
+        text: 'Имя должно быть не менее 3 символов',
+        confirmButtonText: 'Понятно'
+    });
+    return;
+}
 
-    // Проверка валидности почты
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-        alert('Введите корректный email');
-        return;
+        Swal.fire({
+            icon: 'error',
+            title: 'Ошибка валидации',
+            text: 'Введите корректный email',
+            confirmButtonText: 'Понятно'
+        });
     }
 
-    // AJAX запрос
+    if (message.length < 1) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Ошибка валидации',
+            text: 'Сообщение должно быть заполнено',
+            confirmButtonText: 'Понятно'
+        });
+        return;
+    }
     fetch('http://127.0.0.1', {
         method: 'POST',
         headers: {
@@ -30,65 +46,61 @@ document.getElementById('contact-form').addEventListener('submit', function(even
             message
         })
     })
+
     .then(response => response.json())
     .then(data => {
         console.log('Отправлено:', data);
-        alert('Форма отправлена успешно!');
+        Swal.fire({
+            icon: 'success',
+            title: 'Форма успешно отправлена!',
+            confirmButtonText: 'OK'
+        });
     })
-    .catch(error => {
-        console.error('Ошибка:', error);
-        alert('Произошла ошибка при отправке формы');
+
+   .catch(error => {
+    console.error('Ошибка:', error);
+    Swal.fire({
+        icon: 'error',
+        title: 'Ошибка',
+        text: `Произошла ошибка при отправке формы: ${error.message}`,
+        confirmButtonText: 'Понятно'
     });
 });
 
+
+        
 document.getElementById('order-form').addEventListener('submit', function(event) {
     event.preventDefault();
-
-    const name = document.getElementById('customer-name').value.trim();
     const terms = document.getElementById('terms').checked;
-
+    const customername = document.getElementById('customer-name').value.trim();
+    const address = document.getElementById('delivery-address').value.trim();
+    const comment = document.getElementById('comment').value.trim();
     // Проверка обязательных полей
-    if (!name || !terms) {
-        alert('Заполните все обязательные поля');
+    if (!customername || !terms) {
+         Swal.fire({
+        icon: 'error',
+        title: 'Ошибка',
+        text: `Нужно обязательн заполнить имя и подтвердить условия`,
+        confirmButtonText: 'Понятно'
+         })
         return;
     }
-
-    // Отображение SweetAlert
     Swal.fire({
         title: 'Создание заказа',
-        text: `Имя: ${name}`,
+        text: `Имя: ${name}
+        Адресс доставки: ${address},
+        Комментарий к заказу: ${comment}`,
         icon: 'success',
         confirmButtonText: 'OK'
     });
 });
 
-function createReview(name, review) {
-    const reviewElement = document.createElement('div');
-    reviewElement.classList.add('review');
-    reviewElement.innerHTML = `
-        <i class="fas fa-circle"></i>
-        <h3>${name}</h3>
-        <p>${review}</p>
-    `;
-    return reviewElement;
-}
-
-fetch('https://cemen2000.github.io/SmartSoftTask/reviews.json') 
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP ошибка: ${response.status}`);
-        }
-        return response.json();
-    })
+fetch('reviews.json') 
+    .then(response => response.json())
     .then(data => {
-        const reviewContainer = document.querySelector('.review-container'); // Получаем контейнер
-        if (!reviewContainer) {
-            console.error('Контейнер для отзывов не найден!');
-            return;
-        }
-
         data.forEach(item => {
             const reviewElement = createReview(item.name, item.review);
             reviewContainer.appendChild(reviewElement);
         });
     })
+})
